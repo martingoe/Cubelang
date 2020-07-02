fda375a50aa72c7a88f4b85d4be15a3a13719b1f
xCubeArrow <1984flaker@gmail.com>
2020-07-03 00:18:14 +0200

    Create the class Token which is only holding data right now

--- src/main/kotlin/com/cubearrow/cubelang/parsing/tokenization/Token.kt
+++ src/main/kotlin/com/cubearrow/cubelang/parsing/tokenization/Token.kt
@@ -1,33 +1,3 @@
 package com.cubearrow.cubelang.parsing.tokenization
 
-import com.cubearrow.cubelang.bnf.BnfRule
-
-/**
- * The separate tokens in enum form. The same ones can be found in the grammar file.
- */
-enum class Token {
-    IDENTIFIER, IF, RETURN, FUN, WHILE, BRCKTL, BRCKTR, SEMICOLON, ADD, SUB, EXP, DIV, MULT, MOD, EQEQ, EXCLEQ, AND_GATE, OR_GATE, INT_LITERAL, DOUBLE_LITERAL, CHAR_LITERAL, INT_TYPE, DOUBLE_TYPE, INT, EQUALS, NOT_FOUND;
-
-    companion object {
-        /**
-         * Parses a Token from a string using a [TokenGrammar] object to know what RegEx to match.
-         *
-         * @param string       The string to parse the token from
-         * @param tokenGrammar The lexification grammar in [TokenGrammar]. This contains the regexes that the string has to match.
-         * @return Returns the valid Token, returns Token.NOT_FOUND if nothing matches.
-         */
-        fun fromString(string: String, tokenGrammar: TokenGrammar): Token {
-            val rules: MutableList<BnfRule?> = tokenGrammar.bnfParser.rules
-            // Iterate over the grammar entries in order to see which RegEx key matches the string
-            for (rule in rules) {
-                if(rule == null){
-                    continue
-                }
-                if (string.matches(rule.toRegex())) {
-                    return valueOf(rule.name.toUpperCase())
-                }
-            }
-            return NOT_FOUND
-        }
-    }
-}
+class Token (var substring: String, var tokenType: TokenType)
