0e9bd61de3b452116f1dcae08459a84420b4e269
xCubeArrow <1984flaker@gmail.com>
2020-07-03 00:23:02 +0200

    Add the basic structure for a Parser

---
+++ src/main/kotlin/com/cubearrow/cubelang/parsing/syntax/Parser.kt
@@ -0,0 +1,9 @@
+package com.cubearrow.cubelang.parsing.syntax
+
+import com.cubearrow.cubelang.bnf.BnfParser
+import com.cubearrow.cubelang.parsing.tokenization.Token
+import com.cubearrow.cubelang.parsing.tokenization.TokenType
+
+class Parser(tokens: List<Token>, syntaxParser: BnfParser){
+
+}
