21b6c16d3eabd384c6f06518b4cfbce3df8b7c8a
xCubeArrow <1984flaker@gmail.com>
2020-07-26 15:18:37 +0200

    Document ASTGenerator with KDoc

--- src/main/kotlin/com/cubearrow/cubelang/parser/ASTGenerator.kt
+++ src/main/kotlin/com/cubearrow/cubelang/parser/ASTGenerator.kt
@@ -1,13 +1,16 @@
-package com.cubearrow.cubelang.parsing.syntax
+package com.cubearrow.cubelang.parser
 
 import com.cubearrow.cubelang.bnf.BnfParser
 import com.cubearrow.cubelang.bnf.BnfRule
 import com.cubearrow.cubelang.bnf.BnfTerm
-import com.cubearrow.cubelang.parsing.tokenization.Token
-import com.cubearrow.cubelang.parsing.tokenization.TokenGrammar
+import com.cubearrow.cubelang.lexer.Token
+import com.cubearrow.cubelang.lexer.TokenGrammar
 import java.io.File
 import kotlin.system.exitProcess
 
+/**
+ * Initializes an [ASTGenerator] if the arguments are correct. If this is not the case, the correct program usage is printed and the program exits with error code 64.
+ */
 fun main(args: Array<String>) {
     if (args.size != 3) {
         println("Argument usage: <outputDirectory> <SyntaxGrammarFile> <TokenGrammarFile>")
@@ -16,16 +19,33 @@ fun main(args: Array<String>) {
     ASTGenerator(args[0], args[1], args[2])
 }
 
+/**
+ * A generator for the AST from BNF grammar files
+ *
+ * An abstract base class is created that contains the specific subclasses with the appropriate arguments.
+ *
+ * The baseclass also contains a Visitor-Interface which has call functions for every [Expression] type.
+ * These also each implement the abstract [Expression.accept] function calling their specific call function.
+ * The visitor pattern is used to be able to use functions on the specific [Expression] types without changing the generated class.
+ *
+ *
+ * @param outputDir The directory path to output the Expression class to
+ * @param syntaxGrammarFile The path to the bnf syntax grammar file
+ * @param tokenGrammarFile The path to the bnf-token grammar file
+ */
 class ASTGenerator(private val outputDir: String, syntaxGrammarFile: String, tokenGrammarFile: String) {
     private var syntaxGrammarParser: BnfParser
 
+    /**
+     * Initialize the class by creating the grammar objects and generating the actual class
+     */
     init {
         val tokenGrammar = TokenGrammar(File(tokenGrammarFile))
         this.syntaxGrammarParser = BnfParser(File(syntaxGrammarFile), tokenGrammar.bnfParser)
 
         generateExpressionClass()
-
     }
+
 
     private fun generateVisitorInterface(): String {
         var visitorClassContent = "    interface ExpressionVisitor<R> {\n"
@@ -62,12 +82,16 @@ class ASTGenerator(private val outputDir: String, syntaxGrammarFile: String, tok
             """.trimIndent() + "\n"
     }
 
+    /**
+     * Generates the subclasses from the syntax grammar including implementing [Expression.accept]
+     */
     private fun generateSubclasses(): String {
         var expressionFileContent1 = ""
         syntaxGrammarParser.rules.forEach {
             if (it != null && it.name != "expression") {
                 val parameters = expressionToParameters(it.expression)
-                expressionFileContent1 += """   class ${it.name.capitalize()} ($parameters) : Expression() {
+                expressionFileContent1 += """
+    class ${it.name.capitalize()} ($parameters) : Expression() {
         override fun <R> accept(visitor: ExpressionVisitor<R>): R {
             return visitor.visit${it.name.capitalize()}(this)
         }
@@ -79,25 +103,45 @@ class ASTGenerator(private val outputDir: String, syntaxGrammarFile: String, tok
     }
 
 
+    /**
+     * Generates the parameters to the constructor of a subclass of [Expression] from a [List] of [BnfTerm]
+     */
     private fun expressionToParameters(expression: List<List<BnfTerm?>>?): String {
         var result = ""
-        var ruleCount = HashMap<BnfRule, Int>()
+        val ruleCount = HashMap<BnfRule, Int>()
         expression?.get(0)?.forEach { term ->
-            if (term is BnfRule && term.name != "semicolon") {
-                if (ruleCount.containsKey(term)) {
-                    ruleCount[term] = ruleCount[term]!! + 1
-                } else {
-                    ruleCount[term] = 1
-                }
-                result += getTypeFromTerm(term, ruleCount)
+            if (term is BnfRule) {
+                handleRuleCount(ruleCount, term)
+                result += parseSingleParameter(term, ruleCount)
             }
         }
+        // Returns the result while removing ", " from the end
         return result.substring(0, result.length - 2)
     }
 
-    private fun getTypeFromTerm(term: BnfRule, ruleCount: Map<BnfRule, Int>): String {
-        var ruleName: String
+
+    /**
+     * Increment the ruleCount for rules that were already used or initialize it as 1
+     *
+     * The ruleCount is used to avoid duplicate parameter names in the subclasses of [Expression]
+     */
+    private fun handleRuleCount(ruleCount: HashMap<BnfRule, Int>, term: BnfRule) {
+        if (ruleCount.containsKey(term)) {
+            ruleCount[term] = ruleCount[term]!! + 1
+        } else {
+            ruleCount[term] = 1
+        }
+    }
+
+    /**
+     * Parses a single parameter for the constructor for [Expression] subclasses.
+     *
+     * A [MutableList] is used if the name ends with "Lst"
+     */
+    private fun parseSingleParameter(term: BnfRule, ruleCount: Map<BnfRule, Int>): String {
+        val ruleName: String
         val type: String
+        // Handle the names that end with "Lst" to become a MutableList
         if (term.name.indexOf("Lst") == term.name.length - 3) {
             ruleName = term.name.substring(0, term.name.length - 3)
             val rule = syntaxGrammarParser.getRuleFromString(ruleName)
