6aa51744de25220ecdc469a10e0a5e8dc705e7b4
xCubeArrow <1984flaker@gmail.com>
2020-07-26 15:22:18 +0200

    Make Token.kt a data class

---
+++ src/main/kotlin/com/cubearrow/cubelang/lexer/Token.kt
@@ -0,0 +1,3 @@
+package com.cubearrow.cubelang.lexer
+
+data class Token (var substring: String, var tokenType: TokenType, var line: Int, var index: Int)
--- src/main/kotlin/com/cubearrow/cubelang/parsing/tokenization/Token.kt
+++
@@ -1,3 +0,0 @@
-package com.cubearrow.cubelang.parsing.tokenization
-
-class Token (var substring: String, var tokenType: TokenType, var line: Int, var index: Int)
