c2cc1ee1da4f33c1444ad693ad9445e7ee91fdf5
xCubeArrow <1984flaker@gmail.com>
2020-07-29 23:53:39 +0200

    Converted ConsoleColor from being an enum to static values

--- src/main/kotlin/com/cubearrow/cubelang/utils/ConsoleColor.kt
+++ src/main/kotlin/com/cubearrow/cubelang/utils/ConsoleColor.kt
@@ -1,13 +1,15 @@
 package com.cubearrow.cubelang.utils
 
-enum class ConsoleColor(public val ansiCode: String) {
-    ANSI_RESET("\u001B[0m"),
-    ANSI_BLACK("\u001B[30m"),
-    ANSI_RED("\u001B[31m"),
-    ANSI_GREEN("\u001B[32m"),
-    ANSI_YELLOW("\u001B[33m"),
-    ANSI_BLUE("\u001B[34m"),
-    ANSI_PURPLE("\u001B[35m"),
-    ANSI_CYAN("\u001B[36m"),
-    ANSI_WHITE("\u001B[37m");
+class ConsoleColor {
+    companion object {
+        const val ANSI_RESET = "\u001B[0m"
+        const val ANSI_BLACK = "\u001B[30m"
+        const val ANSI_RED = "\u001B[31m"
+        const val ANSI_GREEN = "\u001B[32m"
+        const val ANSI_YELLOW = "\u001B[33m"
+        const val ANSI_BLUE = "\u001B[34m"
+        const val ANSI_PURPLE = "\u001B[35m"
+        const val ANSI_CYAN = "\u001B[36m"
+        const val ANSI_WHITE = "\u001B[37m"
+    }
 }
