35cc059947ce65726972e9ec6a7b35aec831ff77
xCubeArrow <1984flaker@gmail.com>
2020-07-30 23:38:11 +0200

    Add an equals and hashCode function to BnfStringLiteral

--- src/main/kotlin/com/cubearrow/cubelang/bnf/BnfStringLiteral.kt
+++ src/main/kotlin/com/cubearrow/cubelang/bnf/BnfStringLiteral.kt
@@ -5,8 +5,27 @@ class BnfStringLiteral(val regexString: String) : BnfTerm() {
         return Regex(this.regexString)
     }
 
+
     override fun toString(): String {
         return "\"$regexString\""
+    }
+
+    /**
+     * Compares two string literals based on the regexString
+     */
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BnfStringLiteral
+
+        if (regexString != other.regexString) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return regexString.hashCode()
     }
 
 }
