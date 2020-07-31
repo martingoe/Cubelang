db72337c00b3783bd58deddc8240a8932b419152
xCubeArrow <1984flaker@gmail.com>
2020-07-31 17:59:49 +0200

    Update the Tokengrammar and TokenTypes to include comparators

--- src/main/kotlin/com/cubearrow/cubelang/lexer/TokenType.kt
+++ src/main/kotlin/com/cubearrow/cubelang/lexer/TokenType.kt
@@ -6,21 +6,21 @@ import com.cubearrow.cubelang.bnf.BnfRule
  * The separate tokens in enum form. The same ones can be found in the grammar file.
  */
 enum class TokenType {
-    IDENTIFIER, CURLYR, CURLYL, LINE_COMMENT,IF, RETURN, FUN, WHILE, BRCKTL, BRCKTR, SEMICOLON, OPERATOR, EQEQ, EXCLEQ, AND_GATE, OR_GATE, NUMBER, EQUALS, NOT_FOUND, COMMA;
+    IDENTIFIER, CURLYR, CURLYL, LINE_COMMENT, IF, ELSE, RETURN, FUN, WHILE, BRCKTL, BRCKTR, SEMICOLON, OPERATOR, COMPARATOR, AND_GATE, OR_GATE, NUMBER, EQUALS, NOT_FOUND, COMMA;
 
     companion object {
         /**
          * Parses a Token from a string using a [TokenGrammar] object to know what RegEx to match.
          *
          * @param string       The string to parse the token from
-         * @param tokenGrammar The lexification grammar in [TokenGrammar]. This contains the regexes that the string has to match.
+         * @param tokenGrammar The lexer grammar in [TokenGrammar]. This contains the regexes that the string has to match.
          * @return Returns the valid Token, returns Token.NOT_FOUND if nothing matches.
          */
         fun fromString(string: String, tokenGrammar: TokenGrammar): TokenType {
             val rules: MutableList<BnfRule?> = tokenGrammar.bnfParser.rules
             // Iterate over the grammar entries in order to see which RegEx key matches the string
             for (rule in rules) {
-                if(rule == null){
+                if (rule == null) {
                     continue
                 }
                 if (string.matches(rule.toRegex())) {
