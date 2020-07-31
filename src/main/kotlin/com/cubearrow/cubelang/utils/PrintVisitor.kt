7c2cac012a0f7a055f8b2e184f509c7e7b1b84ad
xCubeArrow <1984flaker@gmail.com>
2020-07-31 18:09:46 +0200

    Update the PrintVisitor.kt to include Comparisons and if statements

--- src/main/kotlin/com/cubearrow/cubelang/utils/PrintVisitor.kt
+++ src/main/kotlin/com/cubearrow/cubelang/utils/PrintVisitor.kt
@@ -59,4 +59,25 @@ class PrintVisitor : Expression.ExpressionVisitor<String> {
 
     private fun printExpressionList(list: List<Expression>) =
             list.joinToString(", ", transform = { "{" + it.accept(this) + "}"})
+
+    override fun visitComparison(comparison: Expression.Comparison): String {
+        return """Comparison: {
+    "LeftSide": {
+        ${comparison.expression1.accept(this)}
+    },
+    "Comparator": "${comparison.comparator1.substring}",
+    "RightSide": {
+        ${comparison.expression2.accept(this)}
+    }
+}        """
+    }
+
+    override fun visitIfStmnt(ifStmnt: Expression.IfStmnt): String {
+        return """If-Statement: {
+            |   "Condition": [${ifStmnt.expression1}],
+            |   "Body": [${printExpressionList(ifStmnt.expressionLst1)}],
+            |   "Else-body": [${printExpressionList(ifStmnt.expressionLst2)}]
+            |}
+        """.trimMargin()
+    }
 }
