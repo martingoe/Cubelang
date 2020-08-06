46bf61c245e6ea6fba9565b58b1e67072b38a9f6
xCubeArrow <1984flaker@gmail.com>
2020-08-06 21:48:07 +0200

    Clean up FunctionStorage

--- src/main/kotlin/com/cubearrow/cubelang/interpreter/FunctionStorage.kt
+++ src/main/kotlin/com/cubearrow/cubelang/interpreter/FunctionStorage.kt
@@ -2,29 +2,29 @@ package com.cubearrow.cubelang.interpreter
 
 import com.cubearrow.cubelang.lexer.Token
 import com.cubearrow.cubelang.library.Library
-import com.cubearrow.cubelang.library.PrintingLibrary
 import com.cubearrow.cubelang.main.Main
 import com.cubearrow.cubelang.parser.Expression
 
 class FunctionStorage {
-    class Function(override val name: String, override var args: List<String>, var body: List<Expression>): Callable {
+    class Function(override val name: String, override var args: List<String>, private var body: List<Expression>) : Callable {
         override fun call(variableStorage: VariableStorage, functionStorage: FunctionStorage): Any? {
             return Interpreter(this.body, variableStorage, functionStorage).returnedValue
         }
     }
 
-    var functions = ArrayList<Callable>()
+    private var functions = ArrayList<Callable>()
 
-    fun addFunction(name: Token, args: List<String>, body: List<Expression>){
-        if(functions.stream().anyMatch{it.name == name.substring && it.args == args}){
+    fun addFunction(name: Token, args: List<String>, body: List<Expression>) {
+        if (functions.stream().anyMatch { it.name == name.substring && it.args == args }) {
             Main.error(name.line, name.index, null, "A function with the specified name and argument size already exists")
-        } else{
+        } else {
             functions.add(Function(name.substring, args, body))
         }
     }
-    fun getFunction(name:String, argsSize: Int): Callable? = functions.stream().filter {it.name == name && it.args.size == argsSize}.findFirst().orElse(null)
 
-    init{
+    fun getFunction(name: String, argsSize: Int): Callable? = functions.stream().filter { it.name == name && it.args.size == argsSize }.findFirst().orElse(null)
+
+    init {
         functions.addAll(Library().classes)
     }
 }
