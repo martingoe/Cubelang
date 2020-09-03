82e9fe4d3431e69c3a198e3287595454dc76c106
xCubeArrow <1984flaker@gmail.com>
2020-09-03 22:40:17 +0200

    Update the tests to previous changes

--- src/test/kotlin/com/cubearrow/cubelang/lexer/TokenSequenceTest.kt
+++ src/test/kotlin/com/cubearrow/cubelang/lexer/TokenSequenceTest.kt
@@ -13,7 +13,7 @@ class TokenSequenceTest {
                 Token("=", TokenType.EQUALS, 1, 2),
                 Token("call", TokenType.IDENTIFIER, 1, 5),
                 Token("(", TokenType.BRCKTL, 1, 8),
-                Token("1", TokenType.NUMBER, 1, 9),
+                Token("1", TokenType.DOUBLE, 1, 9),
                 Token(")", TokenType.BRCKTR, 1, 10),
                 Token(";", TokenType.SEMICOLON, 1, 11))
 
