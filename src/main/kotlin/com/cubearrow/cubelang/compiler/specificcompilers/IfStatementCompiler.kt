b6800a60dc0501d2365ec23ec33f1071cca89027
xCubeArrow <1984flaker@gmail.com>
2020-11-22 18:37:47 +0100

    Set the appropriate boolean context values in if statements

--- src/main/kotlin/com/cubearrow/cubelang/compiler/specificcompilers/IfStatementCompiler.kt
+++ src/main/kotlin/com/cubearrow/cubelang/compiler/specificcompilers/IfStatementCompiler.kt
@@ -5,16 +5,17 @@ import com.cubearrow.cubelang.parser.Expression
 
 class IfStatementCompiler(var context: CompilerContext): SpecificCompiler<Expression.IfStmnt> {
     override fun accept(expression: Expression.IfStmnt): String {
-        context.inIfCondition = true
+        context.lIndex++
+        context.inIfStatement = true
+        context.inJmpCondition = true
         val condition = expression.expression1.accept(context.compilerInstance) + "\n"
-        var first = ""
-        context.jmpIfReturnStatement = true
-        first += expression.expression2.accept(context.compilerInstance)
+        context.inJmpCondition = false
 
-        first += if (expression.expressionNull1 != null && !context.separateReturnSegment) "jmp .L${context.lIndex + 1}\n" else ""
-        context.jmpIfReturnStatement = false
+        val first = expression.expression2.accept(context.compilerInstance) + "\n" +
+            if (expression.expressionNull1 != null && !context.separateReturnSegment) "jmp .L${context.lIndex + 1}\n" else ""
 
         val after = ".L${context.lIndex++}:\n${expression.expressionNull1?.accept(context.compilerInstance) ?: ""}"
+        context.inIfStatement = false
         return condition + first + after + if (expression.expressionNull1 != null && !context.separateReturnSegment) "\n.L${context.lIndex}:" else ""
     }
 }
