eded4b89b58168d4bddae495a2adaaa50e462ce7
xCubeArrow <1984flaker@gmail.com>
2020-11-22 18:27:04 +0100

    Add the jmp command when returning from a block

--- src/main/kotlin/com/cubearrow/cubelang/compiler/specificcompilers/BlockCompiler.kt
+++ src/main/kotlin/com/cubearrow/cubelang/compiler/specificcompilers/BlockCompiler.kt
@@ -7,11 +7,10 @@ class BlockCompiler (var context: CompilerContext): SpecificCompiler<Expression.
     override fun accept(expression: Expression.BlockStatement): String {
         var result = ""
         for (it in expression.expressionLst1) {
-            var x = it.accept(context.compilerInstance)
-            result += x + "\n"
+            result += it.accept(context.compilerInstance) + "\n"
             if (it is Expression.ReturnStmnt ) {
-                if(context.inIfCondition) {
-                    x += "\njmp .L${context.lIndex + 1}"
+                if(context.inIfStatement) {
+                    result += "jmp .L${context.lIndex + 1}\n"
                     context.separateReturnSegment = true
                 }
                 break
