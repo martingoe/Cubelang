0c8bd8b72f248a687189da53317fb71ba159b605
xCubeArrow <1984flaker@gmail.com>
2020-11-23 13:49:54 +0100

    Keep the lIndex updated in for loops

--- src/main/kotlin/com/cubearrow/cubelang/compiler/specificcompilers/ForLoopCompiler.kt
+++ src/main/kotlin/com/cubearrow/cubelang/compiler/specificcompilers/ForLoopCompiler.kt
@@ -6,14 +6,15 @@ import com.cubearrow.cubelang.parser.Expression
 class ForLoopCompiler(var context: CompilerContext) : SpecificCompiler<Expression.ForStmnt> {
     override fun accept(expression: Expression.ForStmnt): String {
         context.inJmpCondition = true
+        val contextLIndex = ++context.lIndex
         context.lIndex++
         val x = """${expression.expressionLst1[0].accept(context.compilerInstance)}
-                |.L${context.lIndex++}:
+                |.L${contextLIndex}:
                 |${expression.expressionLst1[1].accept(context.compilerInstance)}
                 |${expression.expression1.accept(context.compilerInstance)}
                 |${expression.expressionLst1[2].accept(context.compilerInstance)}
-                |jmp .L${context.lIndex - 1}
-                |.L${context.lIndex}:""".trimMargin()
+                |jmp .L${contextLIndex}
+                |.L${contextLIndex + 1}:""".trimMargin()
         context.inJmpCondition = false
         return x
     }
