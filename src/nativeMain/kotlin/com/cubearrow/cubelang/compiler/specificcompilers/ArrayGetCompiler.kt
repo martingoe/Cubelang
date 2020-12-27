fe46b058708027f469ec6ed21916e0a2d256ff07
xCubeArrow <1984flaker@gmail.com>
2020-12-27 13:06:57 +0100

    Enable ArrayGets with more than just a VarCall

--- src/nativeMain/kotlin/com/cubearrow/cubelang/compiler/specificcompilers/ArrayGetCompiler.kt
+++ src/nativeMain/kotlin/com/cubearrow/cubelang/compiler/specificcompilers/ArrayGetCompiler.kt
@@ -1,29 +1,25 @@
 package com.cubearrow.cubelang.compiler.specificcompilers
 
-import com.cubearrow.cubelang.compiler.ArrayType
+import Main
 import com.cubearrow.cubelang.compiler.CompilerContext
 import com.cubearrow.cubelang.compiler.CompilerUtils
-import com.cubearrow.cubelang.compiler.Type
 import com.cubearrow.cubelang.parser.Expression
+import com.cubearrow.cubelang.utils.ArrayType
+import com.cubearrow.cubelang.utils.Type
 
-class ArrayGetCompiler(val context: CompilerContext): SpecificCompiler<Expression.ArrayGet> {
+class ArrayGetCompiler(val context: CompilerContext) : SpecificCompiler<Expression.ArrayGet> {
     override fun accept(expression: Expression.ArrayGet): String {
-        if (expression.inBrackets is Expression.Literal) {
+        if (expression.expression is Expression.VarCall && expression.inBrackets is Expression.Literal) {
             val variable = CompilerUtils.getVariableFromArrayGet(expression, context)
             val index = variable!!.index - getIndex(expression, variable.type, variable.type.getRawLength())
             return "[rbp - $index]"
         } else if (expression.expression is Expression.VarCall) {
-            if (expression.inBrackets is Expression.VarCall) {
-                val variable = CompilerUtils.getVariableFromArrayGet(expression.inBrackets, context)
-                val arrayVariable = CompilerUtils.getVariableFromArrayGet(expression, context)
-                if (variable != null) {
-                    val register = CompilerUtils.getRegister("ax", variable.type.getRawLength())
-                    return """mov $register, ${expression.inBrackets.accept(context.compilerInstance)}
-                        |mov rbx, rax
+            val triple = CompilerUtils.moveExpressionToX(expression.inBrackets, context)
+            val arrayVariable = CompilerUtils.getVariableFromArrayGet(expression, context)
+            return """
+                        |movsx rbx, ${triple.second}
                         |[rbp-${arrayVariable!!.index}+rbx*${arrayVariable.type.getRawLength()}]
                     """.trimMargin()
-                }
-            }
         }
         Main.error(-1, -1, null, "Unable to compile the requested type of array access. This may be changed in the future.")
         TODO()
