f24fdda93ebf0a0ee6f496227e3b75c979e22247
xCubeArrow <1984flaker@gmail.com>
2020-12-30 17:05:40 +0100

    Add the operation result type for literals

--- src/nativeMain/kotlin/com/cubearrow/cubelang/compiler/specificcompilers/LiteralCompiler.kt
+++ src/nativeMain/kotlin/com/cubearrow/cubelang/compiler/specificcompilers/LiteralCompiler.kt
@@ -2,12 +2,15 @@ package com.cubearrow.cubelang.compiler.specificcompilers
 
 import com.cubearrow.cubelang.compiler.CompilerContext
 import com.cubearrow.cubelang.parser.Expression
+import com.cubearrow.cubelang.utils.NormalType
 
 class LiteralCompiler(var context: CompilerContext):SpecificCompiler<Expression.Literal> {
     override fun accept(expression: Expression.Literal): String {
         if (expression.value is Int) {
+            context.operationResultType = NormalType("int")
             return expression.value.toString()
         } else if (expression.value is Char) {
+            context.operationResultType = NormalType("char")
             return expression.value.toByte().toInt().toString()
         }
         TODO("Not yet implemented")
