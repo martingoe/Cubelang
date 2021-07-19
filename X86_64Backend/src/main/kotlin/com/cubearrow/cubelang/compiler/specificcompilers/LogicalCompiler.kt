c66ad7fdcd89561bc2c640a8dab3690120e76a5b
Martin Goetze <martinharald.goetze@gmail.com>
2021-07-19 14:15:08 +0200

    Apply a code beautifier to LogicalCompiler.kt

--- X86_64Backend/src/main/kotlin/com/cubearrow/cubelang/compiler/specificcompilers/LogicalCompiler.kt
+++ X86_64Backend/src/main/kotlin/com/cubearrow/cubelang/compiler/specificcompilers/LogicalCompiler.kt
@@ -4,19 +4,19 @@ import com.cubearrow.cubelang.common.Expression
 import com.cubearrow.cubelang.common.tokens.TokenType
 import com.cubearrow.cubelang.compiler.CompilerContext
 
-class LogicalCompiler(val context: CompilerContext): SpecificCompiler<Expression.Logical> {
+class LogicalCompiler(val context: CompilerContext) : SpecificCompiler<Expression.Logical> {
     override fun accept(expression: Expression.Logical): String {
-        if(!context.inJmpCondition)
+        if (!context.inJmpCondition)
             context.error(expression.logical.index, expression.logical.index, "Cannot compile assigning logical expressions yet.")
-        if(expression.logical.tokenType == TokenType.AND){
-            if(context.inJmpCondition){
+        if (expression.logical.tokenType == TokenType.AND) {
+            if (context.inJmpCondition) {
                 return context.evaluate(expression.leftExpression) + context.evaluate(expression.rightExpression) + "\n"
             }
         }
-        if(expression.logical.tokenType == TokenType.OR){
-            if(context.inJmpCondition){
+        if (expression.logical.tokenType == TokenType.OR) {
+            if (context.inJmpCondition) {
                 context.isOR = true
-                val x =  context.evaluate(expression.leftExpression)
+                val x = context.evaluate(expression.leftExpression)
                 context.isOR = false
                 return x + context.evaluate(expression.rightExpression) + "\n"
             }
