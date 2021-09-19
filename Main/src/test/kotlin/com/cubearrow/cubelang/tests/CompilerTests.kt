e3212f212f733ef13985ecbea3158179407c7d38
Martin Goetze <martinharald.goetze@gmail.com>
2021-09-19 16:03:52 +0200

    Add more integration tests

--- Main/src/test/kotlin/com/cubearrow/cubelang/tests/CompilerTests.kt
+++ Main/src/test/kotlin/com/cubearrow/cubelang/tests/CompilerTests.kt
@@ -4,27 +4,57 @@ import org.junit.jupiter.api.Test
 
 class CompilerTests {
     @Test
-    fun fibonacciTest(){
-        assert(getResultOfTest("fibonacci")!! == "832040\n")
+    fun fibonacciTest() {
+        val (s, s1) = getResultOfTest("fibonacci")
+
+        assert(s!! == s1!!)
     }
+
     @Test
-    fun forLoopSumTest(){
-        assert(getResultOfTest("forLoopSum")!! == "55\n")
+    fun forLoopSumTest() {
+        val (s, s1) = getResultOfTest("forLoopSum")
+        assert(s!! == s1!!)
     }
+
     @Test
-    fun pointerDereferencationTest(){
-        assert(getResultOfTest("pointerDereferencation")!! == "10\n")
+    fun pointerDereferencationTest() {
+        val (s, s1) = getResultOfTest("pointerDereferencation")
+        assert(s!! == s1!!)
     }
+
     @Test
-    fun euclideanTest(){
-        assert(getResultOfTest("euclidean")!! == "6\n")
+    fun euclideanTest() {
+        val (s, s1) = getResultOfTest("euclidean")
+        assert(s!! == s1!!)
     }
+
     @Test
-    fun stackArgumentsTest(){
-        assert(getResultOfTest("stackArguments")!! == "98\n")
+    fun stackArgumentsTest() {
+        val (s, s1) = getResultOfTest("stackArguments")
+        assert(s!! == s1!!)
     }
+
     @Test
-    fun structTest(){
-        assert(getResultOfTest("struct")!! == "10\n")
+    fun structTest() {
+        val (s, s1) = getResultOfTest("struct")
+        assert(s!! == s1!!)
+    }
+
+    @Test
+    fun array2dTest() {
+        val (s, s1) = getResultOfTest("2darray")
+        assert(s!! == s1!!)
+    }
+
+    @Test
+    fun arrayToPointerTest() {
+        val (s, s1) = getResultOfTest("arrayToPointer")
+        assert(s!! == s1!!)
+    }
+
+    @Test
+    fun arrayToPointerImplicitConversionTest() {
+        val (s, s1) = getResultOfTest("arrayToPointerImplicitConversion")
+        assert(s!! == s1!!)
     }
 }
--- Main/src/test/kotlin/com/cubearrow/cubelang/tests/TestUtils.kt
+++ Main/src/test/kotlin/com/cubearrow/cubelang/tests/TestUtils.kt
@@ -5,14 +5,14 @@ import java.io.File
 import java.io.IOException
 import java.util.concurrent.TimeUnit
 
-fun getResultOfTest(name: String): String?{
+fun getResultOfTest(name: String): Pair<String?, String?>{
     val pathString = "src/test/resources/testsources/$name"
     val pathFile = File(pathString)
     Main("src/test/resources/library").compileFile(arrayOf("$pathString/source.cube"))
 
     "nasm -f elf64 source.asm ".runCommand(pathFile)
     "gcc source.o -no-pie".runCommand(pathFile)
-    return "./a.out".runCommand(pathFile)
+    return Pair("./a.out".runCommand(pathFile), File("$pathString/expectedResult.txt").readText())
 }
 fun String.runCommand(workingDir: File): String? {
     return try {
